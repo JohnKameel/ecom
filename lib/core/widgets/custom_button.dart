1e8681f74ab21b3d0badfaa4ed2efdb5ee36eaea
JohnKameel <jookameel10@gmail.com>
2024-12-16 22:52:02 +0200

    edits in custom button

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -1,6 +1,10 @@
 import 'package:advancedee/core/utils/size_config.dart';
 import 'package:advancedee/core/widgets/constants.dart';
+import 'package:advancedee/core/widgets/space_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key, this.text, this.onTap});
@@ -28,6 +32,54 @@ class CustomButton extends StatelessWidget {
             ),
             textAlign: TextAlign.left,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButtonWithIcon extends StatelessWidget {
+  const CustomButtonWithIcon(
+      {super.key, required this.text, this.iconData, this.onTap, this.color});
+
+  final String text;
+  final IconData? iconData;
+  final VoidCallback? onTap;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: SizeConfig.screenWidth,
+        decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: Color(0xFF707070),
+            )
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              color: color,
+            ),
+            HorizintalSpace(2),
+            Text(
+              text,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Poppins',
+                fontSize: 12,
+                color: Color(0xff000000),
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ],
         ),
       ),
     );
